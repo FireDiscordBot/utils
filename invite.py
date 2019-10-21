46da472f3b6b4744b56c98c0497837a78112fadc
Jake Ward <thegaminggeek362@gmail.com>
2019-10-21 08:42:25 +0000

    update invite regex

--- invite.py
+++ invite.py
@@ -8,7 +8,7 @@ An extension module to help finding and replacing discord invite links
 import re
 from fire import exceptions
 
-inv = r'(http|https)?(:)?(\/\/)?(discordapp|discord|oh-my-god).(gg|io|me|com|wtf|com\/invite)\/(\w+:{0,1}\w*@)?(\S+)(:[0-10]+)?(\/|\/([\w#!:.?+=&%@!]))?'
+inv = r'(http|https)?(:)?(\/\/)?(invite|discordapp|discord|oh-my-god).(gg|io|me|com|wtf|com\/invite)\/(\w+:{0,1}\w*@)?(\S+)(:[0-10]+)?(\/|\/([\w#!:.?+=&%@!]))?'
 invreplace = '[redacted invite]'
 
 def findinvite(text: str):
