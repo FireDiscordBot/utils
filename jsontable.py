82106a468b709d36aaf7fc2433cc2e76697defca
Jake Ward <thegaminggeek362@gmail.com>
2019-11-17 06:04:46 +0100

    Fix table2json

--- jsontable.py
+++ jsontable.py
@@ -2,7 +2,7 @@ import json
 from bs4 import BeautifulSoup
 
 def table2json(table, keys: list):
-    soup = BeautifulSoup(table, "lxml")
+    soup = BeautifulSoup(table, "html.parser")
     leaderboard = []
     for row in soup.find_all('tr'):
         values = [td.get_text(strip=True) for td in row.find_all('td')]
