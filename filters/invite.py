39f3b02ff286e5f16391a0a46da57f3e78c52588
GamingGeek <geek@gaminggeek.dev>
2020-04-23 19:55:08 +0200

    needs to be non capturing group

--- filters/invite.py
+++ filters/invite.py
@@ -9,7 +9,7 @@ import re
 from fire import exceptions
 
 dgg = r'(?:http|https)?(?::)?(?:\/\/)?discord.(?:gg|io|me)\/([a-zA-Z0-9\-]+)'
-dappcom = r'(?:http|https)?(?::)?(?:\/\/)?discord(app)?.com\/invite\/([a-zA-Z0-9\-]+)'
+dappcom = r'(?:http|https)?(?::)?(?:\/\/)?discord(?:app)?.com\/invite\/([a-zA-Z0-9\-]+)'
 invgg = r'(?:http|https)?(?::)?(?:\/\/)?invite.gg\/([a-zA-Z0-9\-]+)'
 vanity = r'(?:http|https)?(?::)?(?:\/\/)?(?:oh-my-god|inv|floating-through|get-out-of-my-parking|i-live-in|i-need-personal)\.(?:wtf|space)\/([a-zA-Z0-9\-]+)'
 invreplace = '[redacted invite]'
