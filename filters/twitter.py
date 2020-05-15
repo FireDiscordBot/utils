1cf7582525b375e6fec08cb92b9b1a49c4ee9a1a
GamingGeek <geek@gaminggeek.dev>
2020-05-15 20:55:53 +0200

    search for non-special twitter embed (shorteners/anything that returns 301/302)

--- filters/twitter.py
+++ filters/twitter.py
@@ -9,12 +9,16 @@ import re
 from fire import exceptions
 
 twitterurl = r'((?:https?:)?\/\/)?(?:[\w]+\.)?((?:twitter\.com))(\/)([\w]+)(\S+)?'
+twitterembed = r'(?:The latest Tweets from .+ \(@(\w+)\)|.+ \(@(\w+)\) \| Twitter)'
 replace = '[redacted twitter url]'
 
 def findtwitter(text: str):
 	search = re.search(twitterurl, text.strip('<>`*~#!"()[]\{\};:\''))
+	esearch = re.search(twitterembed, text.strip('<>`*~#!"()[]\{\};:\''))
 	if search:
 		return search.group(4)
+	elif esearch:
+		return f'https://twitter.com/{esearch.group(2)}' if esearch.group(2) else f'https://twitter.com/{esearch.group(1)}'
 	else:
 		return False
 
