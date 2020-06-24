bf65a5871ebc1a9e391e756fe9491efb0aa35302
GamingGeek <geek@gaminggeek.dev>
2020-06-24 19:26:01 +0200

    move invite.gg into dgg and add dis.gd

--- filters/invite.py
+++ filters/invite.py
@@ -8,19 +8,18 @@ An extension module to help finding and replacing discord invite links
 import re
 from fire import exceptions
 
-dgg = r'(?:http|https)?(?::)?(?:\/\/)?(discord.(?:gg|io|me)\/([a-zA-Z0-9\-]+))'
+dgg = r'(?:http|https)?(?::)?(?:\/\/)?((?:dis|discord|invite).(?:gd|gg|io|me)\/([a-zA-Z0-9\-]+))'
 dappcom = r'(?:http|https)?(?::)?(?:\/\/)?((?:discord(?:app)?|watchanimeattheoffice).com\/invite\/([a-zA-Z0-9\-]+))'
-invgg = r'(?:http|https)?(?::)?(?:\/\/)?(invite.gg\/([a-zA-Z0-9\-]+))'
 vanity = r'(?:http|https)?(?::)?(?:\/\/)?((?:oh-my-god|(?:h\.|i\.)?inv|floating-through|get-out-of-my-parking|i-live-in|i-need-personal)\.(?:wtf|space)\/([a-zA-Z0-9\-]+))'
 invreplace = '[redacted invite]'
 
 def findinvite(text: str):
 	found = []
-	for r in [dgg, dappcom, invgg, vanity]:
+	for r in [dgg, dappcom, vanity]:
 		[found.append(i) for i in re.findall(r, text, re.MULTILINE) if i and i not in found]
 	return found
 
 def replaceinvite(text: str):
-	for regex in [dgg, dappcom, invgg, vanity]:
+	for regex in [dgg, dappcom, vanity]:
 		text = re.sub(regex, invreplace, text, 0, re.MULTILINE)
 	return text
