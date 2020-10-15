66abe977cea81d03f5c15f2687b89c8435b518ea
Jake Ward <geek@gaminggeek.dev>
2020-10-15 11:59:51 +0100

    change twitter filter

--- filters/twitter.py
+++ filters/twitter.py
@@ -8,23 +8,23 @@ An extension module to help with twitter urls
 import re
 from fire import exceptions
 
-twitterurl = r'((?:https?:)?\/\/)?(?:[\w]+\.)?((?:twitter\.com))(\/)([\w]+)(\S+)?'
-twitterembed = r'(?:The latest Tweets from .+ \(@(\w+)\)|.+ \(@(\w+)\) \| Twitter)'
+twitterurl = r'((?:(?:https?:)?\/\/)?(?:[\w]+\.)?(?:(?:twitter\.com))(?:\/)([\w]+)(\S+)?)'
 replace = '[redacted twitter url]'
 
+
 def findtwitter(text: str):
-	search = re.search(twitterurl, text.strip('<>`*~#!"()[]\{\};:\''))
-	esearch = re.search(twitterembed, text.strip('<>`*~#!"()[]\{\};:\''))
-	if search:
-		return search.group(4)
-	elif esearch:
-		return f'https://twitter.com/{esearch.group(2)}' if esearch.group(2) else f'https://twitter.com/{esearch.group(1)}'
-	else:
-		return False
+    search = re.findall(twitterurl, text.strip(
+        '<>`*~#!"()[]\{\};:\''), re.MULTILINE)
+    if search:
+        return search
+    else:
+        return False
+
 
 def replacetwitter(text: str):
-	message = re.sub(twitterurl, replace, text.strip('<>`*~#!"()[]\{\};:\''), 0, re.MULTILINE)
-	if message:
-		return message
-	else:
-		return False
+    message = re.sub(twitterurl, replace, text.strip(
+        '<>`*~#!"()[]\{\};:\''), 0, re.MULTILINE)
+    if message:
+        return message
+    else:
+        return False
